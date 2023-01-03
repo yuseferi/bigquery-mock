ff14d8f2f7a719bc4c4ffa30bee2c68d4f055d83
Yusef Mohamadi <yusef.mohamadi@deliveryhero.com>
2023-01-03 17:02:51 +0100

    add docs

--- bigquery_mock.go
+++ bigquery_mock.go
@@ -7,6 +7,9 @@ import (
 	"google.golang.org/api/option"
 )
 
+// MockBigQuery is a basic function that create a bigquery mocked client for you.
+// projectName is project name which you wanted to be considered as bigquery project in the mocked service.
+// source is the source data which you want to be added to the mocked service, it could a yaml source file or on the Fly sourcing see https://github.com/yuseferi/bigquery-mock for more information.
 func MockBigQuery(projectName string, sources ...server.Source) (client *bigquery.Client, err error) {
 	ctx := context.Background()
 	bqServer, err := server.New(server.MemoryStorage)
